355fd334e53a10a4ce6b1feb7c70c18e41cb4fee
Patrick Jimison <patrick.jimison@altera.com>
2025-10-13 21:49:03 -0700

    Basic layout implemented

--- main.py
+++ main.py
@@ -6,68 +6,36 @@
 # ///
 
 from textual.app import App, ComposeResult
-from textual.widgets import Markdown
-
-PROMPT_MESSAGE = """\
-## Markdown
-
-- Typography *emphasis*, **strong**, `inline code` etc.    
-- Headers    
-- Lists    
-- Syntax highlighted code blocks
-- Tables and more
-
-## Quotes
-
-> I must not fear.
-> > Fear is the mind-killer.
-> > Fear is the little-death that brings total obliteration.
-> > I will face my fear.
-> > > I will permit it to pass over me and through me.
-> > > And when it has gone past, I will turn the inner eye to see its path.
-> > > Where the fear has gone there will be nothing. Only I will remain.
-
-## Tables
-
-| Name            | Type   | Default | Description                        |
-| --------------- | ------ | ------- | ---------------------------------- |
-| `show_header`   | `bool` | `True`  | Show the table header              |
-| `fixed_rows`    | `int`  | `0`     | Number of fixed rows               |
-| `fixed_columns` | `int`  | `0`     | Number of fixed columns            |
-
-## Code blocks
-
-```python
-def loop_last(values: Iterable[T]) -> Iterable[Tuple[bool, T]]:
-    \"\"\"Iterate and generate a tuple with a flag for last value.\"\"\"
-    iter_values = iter(values)
-    try:
-        previous_value = next(iter_values)
-    except StopIteration:
-        return
-    for value in iter_values:
-        yield False, previous_value
-        previous_value = value
-    yield True, previous_value
-```
+from textual.containers import VerticalScroll
+from textual.widgets import Markdown, Input, Placeholder, Header, Footer
 
 
-"""
-
-
-class Prompt(App):
+class Prompt(Markdown):
 
     def compose(self) -> ComposeResult: 
-        prompt_message = Markdown(PROMPT_MESSAGE)
-        yield prompt_message
+        yield Header()
+        with VerticalScroll(id="chat-view"):
+            yield Response("can a robot learn to love?")
+        yield Input(placeholder="What can I help you with?")
+        yield Footer()
+
 
 class Response(Markdown): 
+    BORDER_TITLE = "Agent TUI"
+
+
+class ModelDetails(Placeholder):
     pass
 
 
+class AgentApp(App): 
+
+    def compose(self) -> ComposeResult:
+        yield Prompt()
+
+
 def main():
-    print("Hello from project-tui!")
-    app = Prompt()
+    app = AgentApp()
     app.run()
 
 
