af72a8afe8c203a3bf2d3640509164378122ecc1
Patrick Jimison <patrick.jimison@altera.com>
2025-10-15 09:54:00 -0700

    Added Input.Submitted decorator to handle text submit events, added work decorator to create a threaded worker for handling responses

--- main.py
+++ main.py
@@ -5,33 +5,53 @@
 # ]
 # ///
 
+from textual import on, work
 from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll
 from textual.widgets import Markdown, Input, Placeholder, Header, Footer
 
 
 class Prompt(Markdown):
+    pass
 
-    def compose(self) -> ComposeResult: 
-        yield Header()
-        with VerticalScroll(id="chat-view"):
-            yield Response("can a robot learn to love?")
-        yield Input(placeholder="What can I help you with?")
-        yield Footer()
 
 
 class Response(Markdown): 
     BORDER_TITLE = "Agent TUI"
 
 
+# TODO - implement widget with details about the model we're using
 class ModelDetails(Placeholder):
     pass
 
 
 class AgentApp(App): 
+    AUTO_FOCUS = "Input"
 
-    def compose(self) -> ComposeResult:
-        yield Prompt()
+    def compose(self) -> ComposeResult: 
+        yield Header()
+        with VerticalScroll(id="chat-view"):
+            yield Response("Hello there")
+        yield Input(placeholder="What can I help you with?")
+        yield Footer()
+
+    def on_mount(self) -> None: 
+        pass
+
+    @on(Input.Submitted)
+    async def on_input(self, event: Input.Submitted) -> None:
+        chat_view = self.query_one("#chat-view")
+        event.input.clear()
+        await chat_view.mount(Prompt(event.value))
+        await chat_view.mount(response := Response())
+        response.anchor()
+        self.send_prompt(event.value, response)
+
+    # TODO - chunk llm response and update response_content
+    @work(thread=True)
+    def send_prompt(self, prompt: str, response: Response) -> None: 
+        response_content = "can a robot learn to love?"
+        self.call_from_thread(response.update, response_content)
 
 
 def main():
